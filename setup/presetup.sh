13c67f26113319260ec7b43fc276b12a11ab6733
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-05-09 19:47:43 +0800

    Auto Backup and Sync - 05-09-2014 19:47:37

--- setup/presetup.sh
+++ setup/presetup.sh
@@ -4,18 +4,31 @@ echo "Note: This is assuming that you have already installed the base system ont
 echo "We will now install the git archives onto the sd card supplied"
 
 
+#Set up environment
+
 pacman -Syu
 pacman -S git udisks udevil
-echo "formatting sdcard"
-mkfs.ext4 /dev/mmcblk0p1
+#echo "formatting sdcard"
+#mkfs.ext4 /dev/mmcblk0p1
 echo "mounting sdcard @ /media/sdcard"
 udevil mount -o ro,noatime /dev/mmcblk0p1 /media/sdcard
+
+
+#Set up ssh
+
+
+
+
+#Set up git folder
 
 git clone git@github.com:jellyjellyrobot/rfidquizgame.git ~/rfidquizgame
 
 echo "running setup scripts"
 
+<<PENDING_DEV
+
 ~/rfidquizgame/setup/
 chmod +x setup.sh #if needed
 ./setup.sh
 
+PENDING_DEV
