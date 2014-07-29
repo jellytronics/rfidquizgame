ec7d4c3a7832f8b32181c57a11e872f22ad72555
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-07-29 16:20:16 +0800

    updated mifare interfae

--- mifare/app.js
+++ mifare/app.js
@@ -10,15 +10,82 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 
 
-var sys = require('sys');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("echo BB-UART1 > /sys/devices/bone_capemgr*/slots", puts);
+
+// MIFARE STUFF
+
+var ndef = require('ndef'), mifare = require('mifare-classic'), message, bytes
+
+function readMifare(){
+  var messageRead;
+  mifare.read(function(err, buffer) {
+    if (err){return}
+    var messageRead = ndef.decodeMessage(buffer.toJSON())
+    console.log(ndef.stringify(messageRead));
+  })
+  return messageRead;
+}
+
+function formatMifare(){
+  mifare.format(function(err) {
+    if (err) {
+      return
+    }
+    console.log("card formatted")
+  })
+}
+
+
+function writeMifare(message){
+  bytes = ndef.encodeMessage(message);
+  mifare.write(bytes, function(err) {
+    if (err) {
+      return
+    }
+    console.log("data written")
+  })
+}
+
+function readCard(answerInstance){
+  var messageRead;
+  mifare.read(function(err, buffer) {
+    if (err){return}
+    var messageRead = ndef.decodeMessage(buffer.toJSON())
+    if (messageRead == undefined || messageRead[0] == undefined) {
+      delete answerInstance.timestamp;
+      delete answerInstance.name;
+      delete answerInstance.teamNumber;
+      delete answerInstance.answerNumber;
+      return}
+    answerInstance.timestamp = new Date().getTime();
+    answerInstance.name = ndef.text.decodePayload(messageRead[0].payload);
+    answerInstance.teamNumber = ndef.text.decodePayload(messageRead[1].payload);
+    answerInstance.answerNumber = ndef.text.decodePayload(messageRead[2].payload);
+    console.log(answerInstance);
+  })
+}
+
+function writeCard(name, teamNumber, answerNumber){
+  message = [
+    ndef.textRecord(name),
+    ndef.textRecord(teamNumber),
+    ndef.textRecord(answerNumber)
+  ]
+  writeMifare(message);
+}
 
 
 
 
 
+// Mongoose
+
+mongoose.connect('mongodb://beagleserver/noob');
+
+
+
+
+
+// CILENT Side
 
 var app = express();
 
@@ -31,99 +98,14 @@ app.get('/', function(req, res){
   res.send('App up and running!');
 });
 
-app.get('/readuid', function(req, res){
+app.get('/readUid', function(req, res){
   res.send();
 });
 
+app.get('/writeMessage', function(req, res){
+  res.send('App up and running!');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var ndef = require('ndef'), mifare = require('mifare-classic'), message, bytes
-
-mifare.read(function(err, buffer) {
-    if (err) {
-        console.log("Read failed ")
-        console.log(err)
-        return
-    }
-    //TODO handle empty buffer!
-    var message = ndef.decodeMessage(buffer.toJSON())
-    console.log("Found NDEF message with " + message.length + (message.length === 1 ? " record" : " records" ))
-    console.log(ndef.stringify(message))
-})
-
-
-
-
-
-
-
-
-console.log(os.hostname())
-
-message = [
-  ndef.textRecord("Hello from nodejs"),
-  ndef.record(ndef.TNF_EXTERNAL_TYPE, 'android.com:pkg' , [], 'com.joelapenna.foursquared')
-]
-
-console.log("noob2")
-
-bytes = ndef.encodeMessage(message)
-
-console.log("noob2a")
-
-/*
-mifare.format(function(err) {
-  if (err) {
-    console.log("Formmating failed")
-    console.log(err)
-    return
-  }
-  console.log("card formatted")
-})
-
-console.log("noob3")
-*/
-
-/*
-mifare.write(bytes, function(err) {
-  if (err) {
-    console.log("Write failed ")
-    console.log(err)
-    return
-  }
-  console.log("data written")
-})
-
-console.log("noob4")
-*/
-
-
-
-mifare.read(function(err, buffer) {
-  if (err) {
-    console.log("Read failed ")
-    console.log(err)
-    return
-  }
-  //TODO handle empty buffer!
-  var message = ndef.decodeMessage(buffer.toJSON())
-  console.log("Found NDEF message with " + message.length + (message.length === 1 ? " record" : " records" ))
-  console.log(ndef.stringify(message))
-})
-
-console.log("noob5")
+app.get('/readUid', function(req, res){
+  res.send();
+});
