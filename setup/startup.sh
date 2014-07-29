043c7870174b8719f7d895fd98abe103a4693cac
Jeremias Wong <jellyjellyrobot@gmail.com>
2014-07-29 17:05:04 +0800

    sleep

--- setup/startup.sh
+++ setup/startup.sh
@@ -30,6 +30,7 @@ echo BB-UART1 > /sys/devices/bone_capemgr*/slots
 if i2cdetect -y -r 1 | grep "27"
     then
     cd ~/rfidquizstash
+    sleep 20
     cp ~/rfidquizgame/hardware/startup_lcd.js ~/rfidquizstash/startup_lcd.js
     node ~/rfidquizstash/startup_lcd.js
 fi
